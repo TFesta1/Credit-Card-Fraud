cb457e58d4f468c17fa281435aa19774aa7107a3
Tanner Festa <festat1@newpaltz.edu>
2024-10-30 00:12:27 -0400

    4. Created Cohorts Page

---
+++ music_controller/frontend/src/components/Cohorts.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { useNavigation } from "react-router-dom";
+
+const Cohorts = () => {
+  const navigation = useNavigation();
+
+  return (
+    <div>
+      <p>Cohorts</p>
+    </div>
+  );
+};
+
+export default Cohorts;
