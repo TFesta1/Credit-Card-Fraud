06becb5ce58f76d8ab8e2aecbcbc0e2451325dd0
Tanner Festa <festat1@newpaltz.edu>
2025-02-15 00:28:54 -0500

    Made initial code load once, and fixed bootstrap spam reloading with moving custom CSS to index.html

--- music_controller/api/apps.py
+++ music_controller/api/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
 from .mlBackend.mlModel import trainModel
-
+import os
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
     def ready(self):
-        trainModel()
+        if os.environ.get('RUN_MAIN'):
+            trainModel()
